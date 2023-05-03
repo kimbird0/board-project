3660a65e89b5b49730588d898c3af91ec65fe8fc
birdzero <redviking2@naver.com>
2023-05-03 23:11:32 +0900

    #8 - 도메인 클래스 설계 (게시글, 댓글)
    기초적인 게시글과 댓글 표현이 가능하도록 함

---
+++ src/main/java/com/project/boardproject/domain/ArticleComment.java
@@ -0,0 +1,15 @@
+package com.project.boardproject.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+
+    private Long id;
+    private Article article;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
